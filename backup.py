acee758279f5820187035119544b71def2c19f45
Eric Bulloch <ebulloch.os@gmail.com>
2024-11-14 14:47:12 -0600

    Added the ability to prune files that are not characters or system files. Added the prune argument to the sample config file

--- backup.py
+++ backup.py
@@ -16,28 +16,35 @@ def get_variables():
             config = json.load(fp)
         print('config.json was successfully loaded.')
 
-    if config.get('source'):
+    if 'source' in config:
         source = config.get('source')
         print(f'Using source path found in config.json ({source})')
     else:
         source = os.path.join(default_base, 'Diablo II Resurrected')
         print(f'Using the default source path ({source})')
 
-    if config.get('destination'):
+    if 'destination' in config:
         destination = config.get('destination')
         print(f'Using destination path found in config.json ({destination})')
     else:
         destination = os.path.join(default_base, 'Backups', 'Diablo II Resurrected')
         print(f'Using the default destination path ({destination})')
 
-    if config.get('timestamp_format'):
+    if 'timestamp_format' in config:
         timestamp_format = config.get('timestamp_format')
         print(f'Using timestamp_format found in config.json ({timestamp_format})')
     else:
         timestamp_format = "%Y.%m.%d.%H.%M.%S"
         print(f'Using the default timestamp_format ({timestamp_format})')
 
-    return source, destination, timestamp_format
+    if 'prune' in config:
+        prune = config.get('prune')
+        print(f'Using prune found in config.json ({prune})')
+    else:
+        prune = True
+        print(f'Using the default prune ({prune})')
+
+    return source, destination, timestamp_format, prune
 
 
 def get_most_recent_backup(destination):
@@ -50,8 +57,33 @@ def get_most_recent_backup(destination):
         return None
 
 
+def prune_files(base_directory):
+    system_files = set(['Settings', 'SharedStashSoftCoreV2'])
+    for directory, directories, file_names in os.walk(base_directory):
+        if not file_names:
+            continue
+        keepers = set([name.split('.')[0] for name in file_names if name.endswith('.map')])
+        keepers.update(system_files)
+        deleted_files = []
+        for file_name in file_names:
+            name = file_name.split('.')[0]
+            if name not in keepers:
+                full_path = os.path.join(directory, file_name)
+                deleted_files.append(full_path)
+                os.remove(full_path)
+        if deleted_files:
+            print(f'Pruned {directory}')
+            for f in deleted_files:
+                print(f' - Deleted {f}')
+
+
 def main():
-    source, base_destination, timestamp_format = get_variables()
+    source, base_destination, timestamp_format, prune = get_variables()
+    if prune:
+        print('Started pruning files')
+        prune_files(source)
+        prune_files(base_destination)
+        print('Finished pruning files')
     most_recent = get_most_recent_backup(base_destination)
     if most_recent:
         print('')
